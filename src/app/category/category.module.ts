c3e0c7086374cfcb401c6568f5c68d42f885dad1
ESDLH <spuvetse@gmail.com>
2022-06-13 13:38:32 +0200

    Finish Front

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,12 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTable, MatTableModule } from '@angular/material/table';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryEditComponent } from './category-edit/category-edit.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CategoryListComponent,
+    CategoryEditComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+
+  providers: [
+    {
+      provide: MAT_DIALOG_DATA,
+      useValue: {},
+    }
   ]
 })
+
 export class CategoryModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,18 +4,31 @@ import { HeaderComponent } from './header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { DialogConfirmationComponent } from './dialog-confirmation/dialog-confirmation.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    DialogConfirmationComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
     MatIconModule,
     MatToolbarModule,
+    MatDialogModule,
+    MatButtonModule
+  ],
+  
+  providers: [
+    {
+      provide: MAT_DIALOG_DATA,
+      useValue: {},
+    }
   ],
   exports: [
     HeaderComponent
